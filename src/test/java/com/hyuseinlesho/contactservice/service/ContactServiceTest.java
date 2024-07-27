9b1d54cc221c1fd5f0af2cb167c0e7a728e99938
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-07-27 11:59:38 +0300

    Add unit tests for ContactService

---
+++ src/test/java/com/hyuseinlesho/contactservice/service/ContactServiceTest.java
@@ -0,0 +1,49 @@
+package com.hyuseinlesho.contactservice.service;
+
+import com.hyuseinlesho.contactservice.dto.CreateContactDto;
+import com.hyuseinlesho.contactservice.mapper.ContactMapper;
+import com.hyuseinlesho.contactservice.model.Contact;
+import com.hyuseinlesho.contactservice.repository.ContactRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ContactServiceTest {
+
+    @Mock
+    private ContactRepository contactRepository;
+
+    @Mock
+    private ContactMapper contactMapper;
+
+    @InjectMocks
+    private ContactService contactService;
+
+    @Test
+    void saveContact() {
+        CreateContactDto contactDto = CreateContactDto.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .message("This is a test message.").build();
+
+        Contact contact = Contact.builder()
+                .name(contactDto.getName())
+                .email(contactDto.getEmail())
+                .message(contactDto.getMessage()).build();
+
+        when(contactMapper.mapToContact(contactDto))
+                .thenReturn(contact);
+        when(contactRepository.save(contact))
+                .thenReturn(contact);
+
+        contactService.saveContact(contactDto);
+
+        verify(contactMapper, times(1)).mapToContact(contactDto);
+        verify(contactRepository, times(1)).save(contact);
+    }
+}
