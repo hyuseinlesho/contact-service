1710dfcdf2eb33dfa9bbf59bcd2ddeaa08650de0
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-07-28 18:02:34 +0300

    Refactor unit tests for ContactService, add new for getNewContactsSince and add integration tests for ContactController

---
+++ src/test/java/com/hyuseinlesho/contactservice/controller/ContactControllerIT.java
@@ -0,0 +1,80 @@
+package com.hyuseinlesho.contactservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyuseinlesho.contactservice.dto.CreateContactDto;
+import com.hyuseinlesho.contactservice.model.Contact;
+import com.hyuseinlesho.contactservice.repository.ContactRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ContactControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        contactRepository.deleteAll();
+    }
+
+    @Test
+    void saveContact() throws Exception {
+        CreateContactDto contactDto = CreateContactDto.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .message("Test message.").build();
+
+        mockMvc.perform(post("/api/contacts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(contactDto)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void getNewContactsSince() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        Contact contact1 = Contact.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .message("Test message")
+                .createdAt(LocalDateTime.now().minusHours(2)).build();
+
+        Contact contact2 = Contact.builder()
+                .name("Test User")
+                .email("test.user@example.com")
+                .message("Another test message")
+                .createdAt(LocalDateTime.now().minusHours(1)).build();
+
+        contactRepository.saveAll(List.of(contact1, contact2));
+
+        String since = now.minusDays(1).format(DateTimeFormatter.ISO_DATE_TIME);
+
+        mockMvc.perform(get("/api/contacts/since")
+                        .param("since", since)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("John Doe"))
+                .andExpect(jsonPath("$[1].name").value("Test User"));
+    }
+}
--- src/test/java/com/hyuseinlesho/contactservice/service/ContactServiceTest.java
+++ src/test/java/com/hyuseinlesho/contactservice/service/ContactServiceTest.java
@@ -3,15 +3,22 @@ package com.hyuseinlesho.contactservice.service;
 import com.hyuseinlesho.contactservice.dto.CreateContactDto;
 import com.hyuseinlesho.contactservice.model.Contact;
 import com.hyuseinlesho.contactservice.repository.ContactRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
-@ExtendWith(MockitoExtension.class)
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class ContactServiceTest {
 
     @Mock
@@ -20,23 +27,52 @@ public class ContactServiceTest {
     @InjectMocks
     private ContactService contactService;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void saveContact() {
         CreateContactDto contactDto = CreateContactDto.builder()
                 .name("John Doe")
                 .email("john.doe@example.com")
-                .message("This is a test message.").build();
-
-        Contact contact = Contact.builder()
-                .name(contactDto.getName())
-                .email(contactDto.getEmail())
-                .message(contactDto.getMessage()).build();
-
-        when(contactRepository.save(contact))
-                .thenReturn(contact);
+                .message("Test message.").build();
 
         contactService.saveContact(contactDto);
 
-        verify(contactRepository, times(1)).save(contact);
+        ArgumentCaptor<Contact> contactArgumentCaptor = ArgumentCaptor.forClass(Contact.class);
+        verify(contactRepository).save(contactArgumentCaptor.capture());
+
+        Contact savedContact = contactArgumentCaptor.getValue();
+        assertThat(savedContact.getName()).isEqualTo("John Doe");
+        assertThat(savedContact.getEmail()).isEqualTo("john.doe@example.com");
+        assertThat(savedContact.getMessage()).isEqualTo("Test message.");
+    }
+
+    @Test
+    void getNewContactsSince() {
+        LocalDateTime since = LocalDateTime.now().minusDays(1);
+
+        Contact contact1 = Contact.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .message("Test message")
+                .createdAt(LocalDateTime.now().minusHours(12)).build();
+
+        Contact contact2 = Contact.builder()
+                .name("Test User")
+                .email("test.user@example.com")
+                .message("Another test message")
+                .createdAt(LocalDateTime.now().minusHours(6)).build();
+
+        when(contactRepository.findContactsSince(any(LocalDateTime.class)))
+                .thenReturn(List.of(contact1, contact2));
+
+        List<Contact> contacts = contactService.getNewContactsSince(since);
+
+        assertEquals(2, contacts.size());
+        assertThat(contacts.get(0).getName()).isEqualTo("John Doe");
+        assertThat(contacts.get(1).getName()).isEqualTo("Test User");
     }
 }
