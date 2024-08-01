a39e0f52bb7353a66380e187f6e1a5fdaff7cc28
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-08-01 13:25:44 +0300

    Update OpenApi configuration

--- src/main/java/com/hyuseinlesho/contactservice/config/OpenApiConfig.java
+++ src/main/java/com/hyuseinlesho/contactservice/config/OpenApiConfig.java
@@ -2,14 +2,24 @@ package com.hyuseinlesho.contactservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
-                description = "ContactService is a RESTful service designed to handle the creation and retrieval of contact messages. " +
-                        "This service is used in conjunction with the PowerLog application to manage user contacts efficiently.",
+                description = "ContactService is a RESTful service designed to handle the creation and retrieval of contact messages. ",
                 title = "ContactService API",
                 version = "1.0"
         )
 )
+@Configuration
 public class OpenApiConfig {
+        @Bean
+        public GroupedOpenApi publicApi() {
+                return GroupedOpenApi.builder()
+                        .group("contacts")
+                        .pathsToMatch("/api/contacts/**")
+                        .build();
+        }
 }
