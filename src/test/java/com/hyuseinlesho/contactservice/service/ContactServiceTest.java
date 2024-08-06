e7871441522c09c14f719a5e2a637e7ee95f4fb1
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-08-06 20:43:21 +0300

    Refactor unit tests

--- src/test/java/com/hyuseinlesho/contactservice/service/ContactServiceTest.java
+++ src/test/java/com/hyuseinlesho/contactservice/service/ContactServiceTest.java
@@ -2,28 +2,31 @@ package com.hyuseinlesho.contactservice.service;
 
 import com.hyuseinlesho.contactservice.model.dto.CreateContactDto;
 import com.hyuseinlesho.contactservice.model.entity.Contact;
+import com.hyuseinlesho.contactservice.producer.ContactProducer;
 import com.hyuseinlesho.contactservice.repository.ContactRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ContactServiceTest {
-    // TODO Refactor tests
 
     @Mock
     private ContactRepository contactRepository;
+
+    @Mock
+    private ContactProducer contactProducer;
 
     @InjectMocks
     private ContactService contactService;
@@ -38,17 +41,31 @@ public class ContactServiceTest {
         CreateContactDto contactDto = CreateContactDto.builder()
                 .name("John Doe")
                 .email("john.doe@example.com")
-                .message("Test message.").build();
+                .message("Test message.")
+                .build();
 
-        contactService.saveContact(contactDto);
+        Contact contact = Contact.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .message("Test message.")
+                .createdAt(LocalDateTime.now())
+                .build();
 
-        ArgumentCaptor<Contact> contactArgumentCaptor = ArgumentCaptor.forClass(Contact.class);
-        verify(contactRepository).save(contactArgumentCaptor.capture());
+        when(contactRepository.save(any(Contact.class))).thenReturn(Mono.just(contact));
 
-        Contact savedContact = contactArgumentCaptor.getValue();
-        assertThat(savedContact.getName()).isEqualTo("John Doe");
-        assertThat(savedContact.getEmail()).isEqualTo("john.doe@example.com");
-        assertThat(savedContact.getMessage()).isEqualTo("Test message.");
+        Mono<Contact> result = contactService.saveContact(contactDto);
+
+        StepVerifier.create(result)
+                .expectNextMatches(savedContact -> {
+                    assertThat(savedContact.getName()).isEqualTo("John Doe");
+                    assertThat(savedContact.getEmail()).isEqualTo("john.doe@example.com");
+                    assertThat(savedContact.getMessage()).isEqualTo("Test message.");
+                    return true;
+                })
+                .expectComplete()
+                .verify();
+
+        verify(contactProducer).sendMessage(contact);
     }
 
     @Test
@@ -59,21 +76,24 @@ public class ContactServiceTest {
                 .name("John Doe")
                 .email("john.doe@example.com")
                 .message("Test message")
-                .createdAt(LocalDateTime.now().minusHours(12)).build();
+                .createdAt(LocalDateTime.now().minusHours(12))
+                .build();
 
         Contact contact2 = Contact.builder()
                 .name("Test User")
                 .email("test.user@example.com")
                 .message("Another test message")
-                .createdAt(LocalDateTime.now().minusHours(6)).build();
+                .createdAt(LocalDateTime.now().minusHours(6))
+                .build();
 
-//        when(contactRepository.findByCreatedAtAfter(any(LocalDateTime.class)))
-//                .thenReturn(List.of(contact1, contact2));
-//
-//        List<Contact> contacts = contactService.getNewContactsSince(since);
-//
-//        assertEquals(2, contacts.size());
-//        assertThat(contacts.get(0).getName()).isEqualTo("John Doe");
-//        assertThat(contacts.get(1).getName()).isEqualTo("Test User");
+        when(contactRepository.findByCreatedAtAfter(since)).thenReturn(Flux.just(contact1, contact2));
+
+        Flux<Contact> result = contactService.getNewContactsSince(since);
+
+        StepVerifier.create(result)
+                .expectNextMatches(contact -> contact.getName().equals("John Doe"))
+                .expectNextMatches(contact -> contact.getName().equals("Test User"))
+                .expectComplete()
+                .verify();
     }
 }
